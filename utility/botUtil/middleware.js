078ee9a23e787f2f071ec9fce69ba3dd6bf9f0ea
Mickeymozy <157571260+Mickeymozy@users.noreply.github.com>
2025-07-11 15:33:01 +0300

    Update middleware.js

--- utility/botUtil/middleware.js
+++ utility/botUtil/middleware.js
@@ -4,13 +4,13 @@ const middleware = async (context, next) => {
     const { m, isBotAdmin, isAdmin } = context;
 
     if (!m.isGroup) {
-        return m.reply("This command is meant for groups");
+        return m.reply("This command is special for groups");
     }
     if (!isAdmin) {
-        return m.reply("You need admin privileges");
+        return m.reply("You need admin promote you");
     }
     if (!isBotAdmin) {
-        return m.reply("I need admin privileges");
+        return m.reply("I need admin promote you");
     }
 
     await next(); // Proceed to the next function (main handler)
