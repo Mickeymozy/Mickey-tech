376329343d2e91d46a8de6714f16c55fa96fcbdb
Mickeymozy <157571260+Mickeymozy@users.noreply.github.com>
2025-07-28 20:31:22 +0300

    Update Menu.js

--- commands/Menu.js
+++ commands/Menu.js
@@ -62,7 +62,7 @@ const detectPlatform = () => {
 
 const fetchRepoStats = async () => {
     try {
-        const response = await axios.get('https://api.github.com/repos/franceking1/Flash-Md-V2');
+        const response = await axios.get('https://api.github.com/repos/Mickeymozy/Mickey-tech');
         const { forks_count, stargazers_count } = response.data;
         return {
             forks: forks_count || 0,
@@ -103,7 +103,7 @@ module.exports = [
                 categorized[category].push(cmd);
             }
 
-            let text = `╭━━━❒ ${applyStyle("FLASH-MD System INFO", 10)} ❒━━━╮\n`;
+            let text = `╭━━━❒ ${applyStyle("MICKEY-TECH System INFO", 10)} ❒━━━╮\n`;
             text += `┃ 🧩 *Commands:* ${allCommands.length.toLocaleString()}\n`;
             text += `┃ 🪄 *Prefix:* ${prefix}\n`;
             text += `┃ ⏰ *Time:* ${time.format('HH:mm:ss')}\n`;
@@ -136,8 +136,8 @@ module.exports = [
                     forwardingScore: 1,
                     isForwarded: true,
                     forwardedNewsletterMessageInfo: {
-                        newsletterJid: '120363238139244263@newsletter',
-                        newsletterName: 'FLASH-MD',
+                        newsletterJid: '120363422552152940@newsletter',
+                        newsletterName: 'MICKEY-TECH',
                         serverMessageId: -1
                     }
                 }
@@ -155,7 +155,7 @@ module.exports = [
         category: 'General',
         execute: async (sock, msg, args, fromJid, allCommands) => {
             const prefix = Array.isArray(config.prefixes) && config.prefixes.length > 0 ? config.prefixes[0] : '.';
-            let text = `*🛠️ FLASH-MD-V2 USER GUIDE*\n\n`;
+            let text = `*🛠️ MICKEY-TECH USER GUIDE*\n\n`;
             text += `To use the bot:\n`;
             text += `• Start commands with the prefix *${prefix}*\n`;
             text += `• Use ${prefix}menu to view all available commands\n`;
@@ -187,8 +187,8 @@ module.exports = [
                     forwardingScore: 1,
                     isForwarded: true,
                     forwardedNewsletterMessageInfo: {
-                        newsletterJid: '120363238139244263@newsletter',
-                        newsletterName: 'FLASH-MD',
+                        newsletterJid: '120363422552152940@newsletter',
+                        newsletterName: 'MICKEY-TECH',
                         serverMessageId: -1
                     }
                 }
